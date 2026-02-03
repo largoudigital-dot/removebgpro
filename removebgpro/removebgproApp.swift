0cf142213e7bda0038bdefda2ed34adeef993300
largoudigital-dot <largou.digital@gmail.com>
2026-02-03 18:39:33 +0100

    Initial Commit

---
+++ removebgpro/removebgproApp.swift
@@ -0,0 +1,17 @@
+//
+//  removebgproApp.swift
+//  removebgpro
+//
+//  Created by Largou on 03.02.26.
+//
+
+import SwiftUI
+
+@main
+struct removebgproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ removebgproTests/removebgproTests.swift
@@ -0,0 +1,17 @@
+//
+//  removebgproTests.swift
+//  removebgproTests
+//
+//  Created by Largou on 03.02.26.
+//
+
+import Testing
+@testable import removebgpro
+
+struct removebgproTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
